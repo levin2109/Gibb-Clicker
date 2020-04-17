d693128eaf45ddc83e539dd7c68d632b37a96436
reischc <cedric.reisch@ict-campus.net>
2020-04-17 11:57:46 +0200

    17.04.2020, 11:57

--- src/sample/Upgrades.java
+++ src/sample/Upgrades.java
@@ -4,8 +4,79 @@ public class Upgrades {
     private String name;
     private String object;
     private int multiplier;
-    private int price;
+    private long price;
     private boolean status;
 
+    /*------------------------------------------------
+                        Constructor
+    ------------------------------------------------*/
+    public Upgrades(String name, String object, int multiplier, long price, boolean status) {
+        this.name = name;
+        this.object = object;
+        this.multiplier = multiplier;
+        this.price = price;
+        this.status = status;
+    }
 
+    /*------------------------------------------------
+                        Methods
+    ------------------------------------------------*/
+    public void activate() {
+        status = true;
+    }
+
+    public void buy(Gibb object) {
+        if (object.money > price) {
+            long balance = object.getMoney();
+            object.setMoney(balance - price);
+        } else {
+            System.out.println("Du hast zu wenig Geld!");
+        }
+    }
+
+    /*------------------------------------------------
+                             Getter
+    ------------------------------------------------*/
+    public boolean isStatus() {
+        return status;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public int getMultiplier() {
+        return multiplier;
+    }
+
+    public String getObject() {
+        return object;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /*------------------------------------------------
+                         Setter
+    ------------------------------------------------*/
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public void setMultiplier(int multiplier) {
+        this.multiplier = multiplier;
+    }
+
+    public void setObject(String object) {
+        this.object = object;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
