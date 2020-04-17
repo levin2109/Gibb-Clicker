37fabdb9a78bbddfe188b9abb9cb08fc9ae3b1c8
reischc <cedric.reisch@ict-campus.net>
2020-04-17 08:13:29 +0200

    Instanzvariabeln Upgrades

--- src/sample/Upgrades.java
+++ src/sample/Upgrades.java
@@ -1,4 +1,11 @@
 package sample;
 
 public class Upgrades {
+    private String name;
+    private String object;
+    private int multiplier;
+    private int price;
+    private boolean status;
+
+
 }
