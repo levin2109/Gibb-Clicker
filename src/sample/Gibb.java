2c9f78d6507dac2674139b35d6c562e946bd32b9
reischc <cedric.reisch@ict-campus.net>
2020-04-30 21:48:22 +0200

    30.04.2020, 21:49

--- src/sample/Gibb.java
+++ src/sample/Gibb.java
@@ -20,19 +20,23 @@ public class Gibb {
     /*------------------------------------------------
                          Methods
     ------------------------------------------------*/
+    //add money for click per second
     public void click() {
         balance = balance + (moneyPerClick*multiplier);
     }
 
+    //load balance from database
     public void loadBalance(String username) {
         UserJDBCDoa User = new UserJDBCDoa();
         balance = User.getBalance(username);
     }
 
+    //add somethin to balance
     public void addBalance(double amount) {
         balance = balance + amount;
     }
 
+    //calculate the money perSecond / 5
     public double calcMoneyPerSecond(List<Tools> toolsList) {
         double money = 0;
         for (Tools tool : toolsList) {
@@ -43,9 +47,26 @@ public class Gibb {
         return money;
     }
 
+    //add mony to balance
     public double addMoney(List<Tools> toolsList) {
         addBalance(calcMoneyPerSecond(toolsList) / 5);
         return calcMoneyPerSecond(toolsList) / 5;
+    }
+
+    //load multiplier per click
+    public void loadMultiplier(List<Upgrades> upgradesList) {
+        String[] powerups = new String[]{"Geld pro Klick erhöhen 1 Mal","Geld pro Klick erhöhen 2 Mal","Geld pro Klick erhöhen 3 Mal","Geld pro Klick erhöhen 4 Mal","Geld pro Klick erhöhen 5 Mal","Geld pro Klick erhöhen 6 Mal","Geld pro Klick erhöhen 7 Mal","Geld pro Klick erhöhen 8 Mal","Geld pro Klick erhöhen 9 Mal","Geld pro Klick erhöhen 10 Mal"};
+        for (Upgrades powerup : upgradesList) {
+            for (int j = 0; j < powerups.length; j++) {
+                if (powerup.getName().equals(powerups[j])) {
+                    if (this.multiplier == 1) {
+                        this.multiplier = powerup.getMultiplier();
+                    } else {
+                        this.multiplier = this.multiplier + powerup.getMultiplier();
+                    }
+                }
+            }
+        }
     }
     /*------------------------------------------------
                           Getter
