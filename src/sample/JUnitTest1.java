2e54d307954702db7b93b5ceeebebf11b4d94032
reischc <cedric.reisch@ict-campus.net>
2020-05-01 09:22:56 +0200

    01.05.2020, 09:23

--- src/sample/JUnitTest1.java
+++ src/sample/JUnitTest1.java
@@ -3,12 +3,20 @@ package sample;
 import org.junit.Test;
 
 public class JUnitTest1 {
+
     @Test
     public void updateMoneyPerSecondTest() {
         long moneyPerSecond = 0;
         long moneyPerSecondNormal = 10;
         int level = 5;
         int multiplier = 2;
+        moneyPerSecond = moneyPerSecondNormal * level * multiplier;
+    }
 
+    @Test
+    public void addBalanceTest() {
+        double balance = 10;
+        double amount = 550;
+        balance = balance + amount;
     }
 }
